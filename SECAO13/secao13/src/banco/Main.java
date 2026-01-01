bd1b89cef9e0c4d92c73620717ceca5db9953ace
igorengsoft <igorsouzasoares1705@gmail.com>
2026-01-01 14:36:03 -0300

    bank simulator

--- SECAO13/secao13/src/banco/Main.java
+++ SECAO13/secao13/src/banco/Main.java
@@ -6,8 +6,9 @@ import java.util.ArrayList;
 public class Main {
 
     static ArrayList<Account> accounts = new ArrayList<>();
-    static String name;
+    static String name = "";
     static Scanner sc = new Scanner(System.in);
+    static String error = "Você digitou algo inválido!";
 
     public static void main(String[] args) {
 
@@ -67,13 +68,15 @@ public class Main {
                 break;
 
             default:
-                System.out.println("Valor inválido!");
+                System.out.println(error);
                 menu();
 
         }
     }
 
     public static void login() {
+
+        Boolean findPerson = false;
 
         System.out.print("Digite o nome da conta: ");
         name = sc.next();
@@ -83,13 +86,16 @@ public class Main {
             if(item.getHolder().equals(name)) {
 
                 name = item.getHolder();
+                findPerson = true;
                 break;
             }
+        }
 
-            else {
+        if (!findPerson) {
 
-                System.out.println("CONTA NÃO ENCONTRADA!");
-            }
+            name = "";
+            System.out.println("CONTA NÃO ENCONTRADA!");
+            login();
         }
     }
 
@@ -110,15 +116,22 @@ public class Main {
             System.out.println("Deseja realizar depósito na nova conta?(Digite 1 para SIM e 2 para NÃO)");
             optionDeposit = sc.nextInt();
 
-            if (optionDeposit == 1) {
+            if (optionDeposit == 2) {
 
                 balance = 0.00;
             }
 
-            else {
+            else if (optionDeposit == 1){
 
                 System.out.println("Qual o valor do depósito?");
                 balance = sc.nextDouble();
+            }
+
+            else {
+
+                registerError();
+                return;
+
             }
 
             if (optionAccount == 1) {
@@ -147,10 +160,16 @@ public class Main {
 
         catch(Exception e) {
 
-            System.out.println("Você digitou algo inválido!");
-            System.out.println("Você terá de iniciar o cadastro novamente!");
-            registerAccount();
+            registerError();
         }
+    }
+
+    public static void registerError() {
+
+        sc.nextLine();
+        System.out.println(error);
+        System.out.println("Você terá de iniciar o cadastro novamente!");
+        registerAccount();
     }
 
     public static void checkBalance() {
@@ -159,7 +178,9 @@ public class Main {
 
             if(item.getHolder().equals(name)) {
 
-                System.out.printf("Saldo atual da conta %s: R$%.2f \n", item.getHolder(), item.getBalance());
+                Double balance = item.getBalance();
+
+                System.out.printf("Saldo atual da conta %s: R$%.2f \n", name, balance);
                 break;
             }
         }
@@ -171,6 +192,13 @@ public class Main {
 
             System.out.print("Digite o valor do depósito: ");
             double amount = sc.nextDouble();
+
+            if(amount <= 0) {
+
+                System.out.println("Digite uma quantidade válida para o depósito");
+                deposit();
+                return;
+            }
 
             for(Account item : accounts) {
 
@@ -185,7 +213,8 @@ public class Main {
 
         catch(Exception e) {
 
-            System.out.println("Você digitou algo inválido!");
+            sc.nextLine();
+            System.out.println(error);
             deposit();
         }
     }
@@ -196,6 +225,13 @@ public class Main {
 
             System.out.print("Digite o valor do saque: ");
             double amount = sc.nextDouble();
+
+            if(amount <= 0) {
+
+                System.out.println("Digite uma quantidade válida para o saque");
+                withdraw();
+                return;
+            }
 
             for(Account item : accounts) {
 
@@ -210,48 +246,69 @@ public class Main {
 
         catch(Exception e) {
 
-            System.out.println("Você digitou algo inválido!");
+            sc.nextLine();
+            System.out.println(error);
             withdraw();
         }
     }
 
     public static void transfer() {
 
+        if (accounts.size() < 2) {
+
+            System.out.println("É necessário haver ao menos duas contas cadastradas");
+            return;
+        }
+
         try {
+
+            String person;
+            boolean find1 = false;
+            boolean find2 = false;
+            String message = "Conta não encontrada!";
 
             System.out.print("Digite o valor da transferência: ");
             double amount = sc.nextDouble();
 
             System.out.println("Digite o nome da pessoa para quem irá transferir: ");
-            String person = sc.next();
+            person = sc.next();
 
             for(Account item : accounts) {
 
                 if (item.getHolder().equals(person)) {
 
                   item.setBalance(item.getBalance() + amount);
-
-                  for (Account item2 : accounts) {
-
-                      if (item2.getHolder().equals(name)) {
-
-                          item2.setBalance(item2.getBalance() - amount);
-                          break;
-                      }
-                  }
+                  find1 = true;
                   break;
                 }
+            }
 
-                else {
+            if (find1 == false) {
 
-                    System.out.println("Conta não encontrada!");
+                    System.out.println(message);
+                    return;
                 }
+
+            for (Account item2 : accounts) {
+
+                if (item2.getHolder().equals(name)) {
+
+                    item2.setBalance(item2.getBalance() - amount);
+                    find2 = true;
+                    break;
+                }
+            }
+
+            if (find2 == false) {
+
+                System.out.println(message);
             }
         }
 
         catch(Exception e) {
 
-            System.out.println("Você digitou algo inválido!");
+            sc.nextLine();
+            System.out.println(error);
             transfer();
         }
     }
